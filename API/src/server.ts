b4b58c16be870ec39c3e9bd0d1159ce91bc11cf0
fawall <fabiolopes7844@gmail.com>
2021-02-23 13:34:08 -0300

    using routes in server.ts

--- API/src/server.ts
+++ API/src/server.ts
@@ -1,13 +1,13 @@
+import 'reflect-metadata';
 import express from 'express';
+import './database';
+import { router } from './routes';
 
 const app = express();
 
+app.use(express.json());
+app.use(router)
+
 app.listen(3333, () => console.log("Server Is Running"));
 
-app.get("/", (req, res ) =>{
-    return res.send("Hello Word");
-}); 
 
-app.post("/", (req, res) => {
-    return res.json({message: "Os dados foram salvos com sucesso"})
-});
